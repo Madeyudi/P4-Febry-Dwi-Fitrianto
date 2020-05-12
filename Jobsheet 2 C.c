23baa4fee4f59b94e3ebef7e35e603dfa4501875
I Made Yudi Dharmawan <64619630+Madeyudi@users.noreply.github.com>
2020-05-12 21:59:48 +0700

    Create Jobsheet 2 C.c
    Mengisi dan menampilkan element array (Dinamis)

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+main (){
+	int nim[7];
+	printf(" Nama : I Made Yudi Dharmawan\n");
+	printf(" NIM : F1B019067\n");
+	printf(" Kelompok : 15 \n");
+printf ("Masukkan nilai ke-1 = ");
+scanf ("%i", &nim[0]);
+printf ("Masukkan nilai ke-2 = ");
+scanf ("%i", &nim[1]);
+printf ("Masukkan nilai ke-3 = ");
+scanf ("%i", &nim[2]);
+printf ("Masukkan nilai ke-4 = ");
+scanf ("%i", &nim[3]);
+printf ("Masukkan nilai ke-5 = ");
+scanf ("%i", &nim[4]);
+printf ("Masukkan nilai ke-6 = ");
+scanf ("%i", &nim[5]);
+printf ("Masukkan nilai ke-7 = ");
+scanf ("%i", &nim[6]);
+	
+
+	printf("\n");
+	
+	printf("Nilai ke-1 = %i\n",nim[0]);
+	printf("Nilai ke-2 = %i\n",nim[1]);
+	printf("Nilai ke-3 = %i\n",nim[2]);
+	printf("Nilai ke-4 = %i\n",nim[3]);
+	printf("Nilai ke-5 = %i\n",nim[4]);
+	printf("Nilai ke-6 = %i\n",nim[5]);
+	printf("Nilai ke-7 = %i\n",nim[6]);
+}
