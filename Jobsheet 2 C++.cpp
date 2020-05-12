47044829544cdf7b0d015537ef258a9b36a36253
I Made Yudi Dharmawan <64619630+Madeyudi@users.noreply.github.com>
2020-05-12 22:00:29 +0700

    Create Jobsheet 2 C++.cpp
    Mengisi dan menampilkan element array (Dinamis)

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+main (){
+	int nim[7];
+	cout<<" Nama : I Made Yudi Dharmawan\n";
+	cout<<" NIM : F1B019067\n";
+	cout<<" Kelompok : 15 \n";
+cout << "Masukkan nilai ke-1 = ";
+cin >> nim[0];
+cout << "Masukkan nilai ke-2 = ";
+cin >> nim[1];
+cout << "Masukkan nilai ke-3 = ";
+cin >> nim[2];
+cout << "Masukkan nilai ke-4 = ";
+cin >> nim[3];
+cout << "Masukkan nilai ke-5 = ";
+cin >> nim[4];
+cout << "Masukkan nilai ke-6 = ";
+cin >> nim[5];
+cout << "Masukkan nilai ke-7 = ";
+cin >> nim[6];
+
+
+	cout<<"\n";
+	
+	cout<<"Nilai ke-1 = "<<nim[0]<<endl;
+	cout<<"Nilai ke-2 = "<<nim[1]<<endl;
+	cout<<"Nilai ke-3 = "<<nim[2]<<endl;
+	cout<<"Nilai ke-4 = "<<nim[3]<<endl;
+	cout<<"Nilai ke-5 = "<<nim[4]<<endl;
+	cout<<"Nilai ke-6 = "<<nim[5]<<endl;
+	cout<<"Nilai ke-7 = "<<nim[6]<<endl;
+}
