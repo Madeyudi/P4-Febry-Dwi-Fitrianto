21bb24ac17b298a4328aea3f4f06ed9129155d7e
I Made Yudi Dharmawan <64619630+Madeyudi@users.noreply.github.com>
2020-05-12 21:58:23 +0700

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Nama : I Made Yudi Dharmawan" << endl;
+	cout<<"NIM : 7"<<endl;
+	cout<<"Kelompok : 15"<<endl;
+int nim[7]= {7,6,0,9,1,3,5};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+	cout << "Nim ke-6 adalah " << nim[5] << endl;
+	cout << "Nim ke-7 adalah " << nim[6] << endl;
+}
