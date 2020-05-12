9ebf7f93244abd8bce1b2f98ec5b9bd95d83497a
I Made Yudi Dharmawan <64619630+Madeyudi@users.noreply.github.com>
2020-05-12 21:57:52 +0700

    Create Jobsheet 1 C.c
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	printf("\nNama : I Made Yudi Dharmawan");
+	printf("\nNIM : 7");
+	printf("\nKelompok : 15");
+	int nim[7] = {7,6,0,9,1,3,5};
+	
+	printf("\nNIM ke-1 = %d",nim[0]);
+	printf("\nNIM ke-2 = %d",nim[1]);
+	printf("\nNIM ke-3 = %d",nim[2]);
+	printf("\nNIM ke-4 = %d",nim[3]);
+	printf("\nNIM ke-5 = %d",nim[4]);
+	printf("\nNIM ke-6 = %d",nim[5]);
+	printf("\nNIM ke-7 = %d",nim[6]);
+}
